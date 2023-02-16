2ac862aaa32009fa50260f3afd86c1d6444ee095
feifei <864047189@qq.com>
2023-02-16 11:03:45 +0800

    热修复

--- src/main/java/com/atguigu/HotFix.java
+++ src/main/java/com/atguigu/HotFix.java
@@ -3,5 +3,8 @@ package com.atguigu;
 public class HotFix {
     public static void main(String[] args) {
         System.out.println("hotfixing");
+        System.out.println("master1");
+        System.out.println("hotfix1");
+
     }
 }
